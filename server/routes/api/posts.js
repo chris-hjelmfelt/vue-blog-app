709438d5f46ec134ec1cb671d6cec6a1ad19cc22
Chris Hjelmfelt <christine.hjelmfelt@gmail.com>
2021-05-20 10:30:39 -0600

    delete posts

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -19,11 +19,19 @@ router.post('/', async (req, res) => {  // slash references where we are now not
   res.status(201).send();
 });  
 
-// testing with no front end - put the following into Powershell and change the text to get a new post
-// curl --% --request POST --header "Content-Type: application/json" --data  "{\"text\":\"I like Vue\"}" localhost:3000/api/posts
+// testing with no front end - put the following into Git Bash (see README for powershell notes) and change the text to get a new post
+// curl --request POST --header "Content-Type: application/json"  --data '{"text":"I am learning Curl"}' localhost:3000/api/posts
 
 
 // Delete Post
+router.delete('/:id', async (req, res) => {
+  const posts = await loadPostsCollection();
+  await posts.deleteOne({_id: new mongodb.ObjectID(req.params.id)});
+  res.status(200).send();
+});
+
+// curl -X DELETE localhost:3000/api/posts/60a592751c83c23b18403b19
+
 
 
 // connect to the mongodb database
