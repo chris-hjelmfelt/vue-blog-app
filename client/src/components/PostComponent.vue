b51dcd9360186b0039760f5d7f4bd2b978cc13fd
Chris Hjelmfelt <christine.hjelmfelt@gmail.com>
2021-05-20 17:56:03 -0600

    add new posts  and delete posts

--- client/src/components/PostComponent.vue
+++ client/src/components/PostComponent.vue
@@ -1,10 +1,15 @@
 <template>
   <div class="container">
     <h1>Posts</h1>
+    <div class="create-posts">
+      <label for="create-posts">Create a new post</label><br />
+      <input type="text" id="createpost" v-model="text" placeholder="say something">
+      <button v-on:click="createPost" value="Submit">Post</button>
+    </div>
     <hr>
     <p class="error" v-if="error">{{ error }}</p>
     <div class="posts-continer"></div>
-    <div class="post" v-for="post in posts" v-bind:item="post" v-bind:key="post._id">
+    <div class="post" v-for="post in posts" v-bind:item="post" v-bind:key="post._id" v-on:dblclick=deletePost(post._id)>
       {{ post.createdAt.getMonth()+1 }} / {{ post.createdAt.getDate() }} / {{ post.createdAt.getFullYear() }}
       <p class="text">{{ post.text }}</p>
     </div>
@@ -28,6 +33,17 @@ export default {
       this.posts = await PostService.getPosts();
     } catch(err) {
       this.error = err.message;
+    }
+  },
+  methods: {
+    async createPost() {
+      await PostService.insertPost(this.text);
+      this.posts = await PostService.getPosts();
+      this.text = "";
+    },
+     async deletePost(id) {
+      await PostService.deletePost(id);
+      this.posts = await PostService.getPosts();
     }
   }
 }
@@ -58,5 +74,8 @@ export default {
       font-size: 22px; 
       font-weight: 700; 
       margin-bottom: 0; 
+    }
+    #createpost {
+      width: 300px;
     }
 </style>
