107c2ba4d59eef410984599795ff761391bb5b4b
Chris Hjelmfelt <christine.hjelmfelt@gmail.com>
2021-06-04 19:37:29 -0600

    getting a list of what tags are being used and displaying them beside the blog

--- client/src/components/PostComponent.vue
+++ client/src/components/PostComponent.vue
@@ -1,33 +1,40 @@
 <template>
-  <div class="container">    
-    <h1>Posts</h1>
-    <!-- The form for new posts -->
-    <button class="openbtn" v-on:click="openNav()">Create a new post</button>
-    <div class="create-posts" ref="jedi">
-      <input type="text" id="createtitle" v-model="title" placeholder="say something"><br />
-      <textarea type="text" id="createpost" v-model="post_body" rows="6" cols="30">
-      </textarea><br />
-      <button v-on:click="createPost" value="Submit">Post</button>
+  <div class="all">
+    <div class="container">    
+      <h1>Posts</h1>
+      <!-- The form for new posts -->
+      <button class="openbtn" v-on:click="openNav()">Create a new post</button>
+      <div class="create-posts" ref="create">
+        <input type="text" id="createtitle" v-model="title" placeholder="say something"><br />
+        <textarea type="text" id="createpost" v-model="post_body" rows="6" cols="30">
+        </textarea><br />
+        <button v-on:click="createPost" value="Submit">Post</button>
+      </div>
+      <hr>
+      <!-- The Posts start here -->
+      <p class="error" v-if="error">{{ error }}</p>
+      <div class="post" v-for="post in posts" v-bind:item="post" v-bind:key="post.post_id" v-on:dblclick=deletePost(post.post_id)>
+        <div class="post-info">
+          {{ post.post_date }} 
+          <p class="text">{{ post.author }} </p>        
+        </div> 
+        <div class="main-body">    
+          <div class="message">    
+            <p class="text">{{ post.title }}</p>
+            <p class="text">{{ post.post_body }}</p>
+          </div>
+          <div class="tags">
+            Tags: {{ post.tags.tags }}
+          </div>
+        </div>           
+      </div>     
     </div>
-    <hr>
-    <!-- The Posts start here -->
-    <p class="error" v-if="error">{{ error }}</p>
-    <div class="post" v-for="post in posts" v-bind:item="post" v-bind:key="post.post_id" v-on:dblclick=deletePost(post.post_id)>
-      <div class="post-info">
-        {{ post.post_date }} 
-        <p class="text">{{ post.author }} </p>        
-      </div> 
-      <div class="main-body">    
-        <div class="message">    
-          <p class="text">{{ post.title }}</p>
-          <p class="text">{{ post.post_body }}</p>
-        </div>
-        <div class="tags">
-          Tags: {{ post.tags.tags }}
-        </div>
-      </div>           
+    <div class="all-tags">
+      <h4>Tags:</h4>
+      <p ref="mytags"> </p>
     </div>
   </div>
+  
 </template>
 
 
@@ -49,23 +56,31 @@ export default {
   async created() {
     try {
       this.posts = await PostService.getPosts();
+      console.log(this.posts);
+      this.setTags();
     } catch(err) {
       this.error = err.message;
     }
   },
   methods: {
     openNav() {   /* Show the create post form */
-      if (this.$refs.jedi.style.height == "250px"){
-        this.$refs.jedi.style.height = "0px";
+      if (this.$refs.create.style.height == "250px"){
+        this.$refs.create.style.height = "0px";
       } else {
-        this.$refs.jedi.style.height = "250px";
+        this.$refs.create.style.height = "250px";
       }  
+    },
+    setTags() {
+      var tagValues = this.posts.map( (value) => value.tags.tags).filter( (value, index, _arr) => _arr.indexOf(value) == index);
+      console.log(tagValues);
+      this.$refs.mytags.innerHTML = tagValues.toString();
     },
     async createPost() {
       await PostService.insertPost(this.title, this.post_body);
       this.posts = await PostService.getPosts();
       this.title = "";   // clears the input field after posting
       this.post_body = "";
+      this.openNav();
     },
      async deletePost(id) {
       await PostService.deletePost(id);
@@ -80,10 +95,16 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+    div.all {
+      width: 1000px;
+      margin: 0 auto; 
+    }
+
     div.container { 
       max-width: 800px; 
       margin: 0 auto; 
       background-color: #fff;
+      float: left;
     }
      
     div.create-posts {       
@@ -134,5 +155,11 @@ export default {
     .tags {
       font-size: 80%;
       height: 10px;      
+    }
+
+    div.all-tags {
+      width: 190px;
+      float: left;
+      border: 1px solid black;
     }
 </style>
